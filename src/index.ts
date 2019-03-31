4710c32e40fe904b36efce5262e354caa89d1595
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-03-30 23:44:15 -0400

    Add comments, watch for storage event as well

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 
-interface KVP<K,V> {
+
+interface KVP<K, V> {
   key: K,
   value: V
 }
 
-export class LocalStorageChanged extends CustomEvent<KVP<string, string>> {
+
+class LocalStorageChanged extends CustomEvent<KVP<string, string>> {
   static eventName = 'onLocalStorageChange';
 
   constructor(payload: KVP<string, string>) {
@@ -13,30 +15,76 @@ export class LocalStorageChanged extends CustomEvent<KVP<string, string>> {
   }
 }
 
+
+/**
+ * Use this instead of directly using localStorage.setItem
+ * in order to correctly send events within the same window.
+ * 
+ * @example
+ * ```js
+ * writeStorage('hello', JSON.stringify({ name: 'world' }));
+ * const { name } = JSON.parse(localStorage.getItem('hello'));
+ * ```
+ * 
+ * @export
+ * @param {string} key The key to write to in the localStorage.
+ * @param {string} value The value to write to in the localStorage.
+ */
 export function writeStorage(key: string, value: string) {
   localStorage.setItem(key, value);
-  window.dispatchEvent(new LocalStorageChanged({key, value}));
+  window.dispatchEvent(new LocalStorageChanged({ key, value }));
 }
 
+
+/**
+ * React hook to enable updates to state via localStorage.
+ * This updates when the {writeStorage} function is used, when the returned function
+ * is called, or when the "storage" event is fired from another tab in the browser.
+ * 
+ * @example
+ * ```js
+ * const MyComponent = () => {
+ *   const [myStoredItem, setMyStoredItem] = useLocalStorage('myStoredItem');
+ *   return (
+ *     <p>{myStoredItem}</p>
+ *   );
+ * };
+ * ```
+ * 
+ * @export
+ * @param {string} key The key in the localStorage that you wish to watch.
+ * @returns An array containing the value associated with the key in position 0,
+ * and a function to set the value in position 1.
+ */
 export function useLocalStorage(key: string) {
   const [localState, updateLocalState] = useState(localStorage.getItem(key));
-  
-  function onLocalStorageChange(event: LocalStorageChanged) {
-    if (event.detail.key === key) {
-      updateLocalState(event.detail.value);
+
+  function onLocalStorageChange(event: LocalStorageChanged | StorageEvent) {
+    if (event instanceof LocalStorageChanged) {
+      if (event.detail.key === key) {
+        updateLocalState(event.detail.value);
+      }
+    } else {
+      if (event.key === key) {
+        updateLocalState(event.newValue);
+      }
     }
   }
 
-  // Filters out the events by the key passed into useLocalStorage.
   useEffect(() => {
     window.addEventListener(
-      LocalStorageChanged.eventName, 
+      LocalStorageChanged.eventName,
       (e: any) => onLocalStorageChange(e as LocalStorageChanged)
     );
-    return () => window.removeEventListener(
-      LocalStorageChanged.eventName,
-      (e: any) => onLocalStorageChange(e)
-    );
+    window.addEventListener('storage', e => onLocalStorageChange(e));
+
+    return () => {
+      window.removeEventListener(
+        LocalStorageChanged.eventName,
+        (e: any) => onLocalStorageChange(e as LocalStorageChanged)
+      );
+      window.removeEventListener('storage', e => onLocalStorageChange(e));
+    };
   }, []);
 
   return [localState, (value: string) => writeStorage(key, value)];
