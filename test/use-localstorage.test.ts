d5e413425cc70fa34bd82daf2699de8680bf4a22
Fatih Kaya <1994274@gmail.com>
2019-10-20 20:57:41 +0300

    add test to check default value to not override existing data

--- test/use-localstorage.test.ts
+++ test/use-localstorage.test.ts
@@ -8,5 +8,21 @@ describe('Module: use-localstorage', () => {
             
             expect(result.current).toBeDefined();
         });
+
+        it('does not override existing data', () => {
+            const key = `dynamickey-` + Date.now()
+            const firstDefaultValue = Date.now()
+
+            // first call of the hook
+            renderHook(() => useLocalStorage(key, firstDefaultValue));
+
+            // second render. as the value already set, default value
+            // should not override existing value.
+            const { result: result2 } = renderHook(() => useLocalStorage(key, Date()));
+
+            const [lastValue] = result2.current
+
+            expect(lastValue).toEqual(firstDefaultValue);
+        });
     });
 });
