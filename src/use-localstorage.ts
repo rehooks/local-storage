7cf2f3bad1894822c53d84eddd967abdd60709f2
Harley Alexander <43975092+mayteio@users.noreply.github.com>
2020-02-19 18:20:48 +1100

    Fix stale localState (#44)
    * Fix stale localState when key changes externally, added test
    
    * update onLocalStorageChange dependency to include key, subsequent calls would be stale.

--- src/use-localstorage.ts
+++ src/use-localstorage.ts
@@ -1,4 +1,9 @@
-import { writeStorage, deleteFromStorage, LocalStorageChanged, isTypeOfLocalStorageChanged } from './local-storage-events';
+import {
+  writeStorage,
+  deleteFromStorage,
+  LocalStorageChanged,
+  isTypeOfLocalStorageChanged,
+} from './local-storage-events';
 import { useEffect, useState, Dispatch, useCallback } from 'react';
 
 function tryParse(value: string) {
@@ -24,39 +29,49 @@ function tryParse(value: string) {
  *   );
  * };
  * ```
- * 
+ *
  * @export
  * @template TValue The type of the given initial value.
  * @param {string} key The key in the localStorage that you wish to watch.
  * @param {TValue} initialValue Optional initial value to start with.
- * @returns {[TValue | null, Dispatch<TValue>, Dispatch<void>]} An array containing the value 
+ * @returns {[TValue | null, Dispatch<TValue>, Dispatch<void>]} An array containing the value
  * associated with the key in position 0, a function to set the value in position 1,
  * and a function to delete the value from localStorage in position 2.
  */
-export function useLocalStorage<TValue = string>(key: string, initialValue?: TValue): [TValue | null, Dispatch<TValue>, Dispatch<void>] {
-  const [localState, updateLocalState] = useState(tryParse(localStorage.getItem(key)!) || initialValue);
+export function useLocalStorage<TValue = string>(
+  key: string,
+  initialValue?: TValue
+): [TValue | null, Dispatch<TValue>, Dispatch<void>] {
+  const [localState, updateLocalState] = useState(
+    tryParse(localStorage.getItem(key)!) || initialValue
+  );
 
-  const onLocalStorageChange = useCallback((event: LocalStorageChanged<TValue> | StorageEvent) => {
-    if (isTypeOfLocalStorageChanged(event)) {
-      if (event.detail.key === key) {
-        updateLocalState(event.detail.value);
-      }
-    } else {
-      if (event.key === key) {
-        if (event.newValue) {
-          updateLocalState(tryParse(event.newValue));
+  const onLocalStorageChange = useCallback(
+    (event: LocalStorageChanged<TValue> | StorageEvent) => {
+      if (isTypeOfLocalStorageChanged(event)) {
+        if (event.detail.key === key) {
+          updateLocalState(event.detail.value);
+        }
+      } else {
+        if (event.key === key) {
+          if (event.newValue) {
+            updateLocalState(tryParse(event.newValue));
+          }
         }
       }
-    }
-  }, [updateLocalState]);
-
+    },
+    [updateLocalState, key]
+  );
 
   useEffect(() => {
-    // The custom storage event allows us to update our component 
+    updateLocalState(tryParse(localStorage.getItem(key)!) || initialValue);
+  }, [key]);
+
+  useEffect(() => {
+    // The custom storage event allows us to update our component
     // when a change occurs in localStorage outside of our component
-    window.addEventListener(
-      LocalStorageChanged.eventName,
-      (e: any) => onLocalStorageChange(e as LocalStorageChanged<TValue>)
+    window.addEventListener(LocalStorageChanged.eventName, (e: any) =>
+      onLocalStorageChange(e as LocalStorageChanged<TValue>)
     );
 
     // The storage event only works in the context of other documents (eg. other browser tabs)
@@ -72,20 +87,15 @@ export function useLocalStorage<TValue = string>(key: string, initialValue?: TVa
     }
 
     return () => {
-      window.removeEventListener(
-        LocalStorageChanged.eventName,
-        (e: any) => onLocalStorageChange(e as LocalStorageChanged<TValue>)
+      window.removeEventListener(LocalStorageChanged.eventName, (e: any) =>
+        onLocalStorageChange(e as LocalStorageChanged<TValue>)
       );
       window.removeEventListener('storage', e => onLocalStorageChange(e));
     };
-  }, []);
+  }, [key]);
 
   const writeState = useCallback((value: TValue) => writeStorage(key, value), [key]);
   const deleteState = useCallback(() => deleteFromStorage(key), [key]);
 
-  return [
-    localState === null ? initialValue : localState,
-    writeState,
-    deleteState
-  ];
+  return [localState === null ? initialValue : localState, writeState, deleteState];
 }
