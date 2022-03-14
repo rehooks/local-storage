8f60a60ffbffd1cc54d24b14f2b045c03c5389cc
Eduardo Fortuna <eduardofortuna99@gmail.com>
2022-03-14 00:09:14 -0600

    Simplify import/export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { useLocalStorage } from './use-localstorage';
+export { useLocalStorage } from './use-localstorage';
 
 export { writeStorage, deleteFromStorage } from './local-storage-events';
-
-export { useLocalStorage };
 
 export default useLocalStorage;
